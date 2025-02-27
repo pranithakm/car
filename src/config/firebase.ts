c9df470c0d43ab7343fd7bb4bba42efa8acd06a3
pranithakm <149092942+pranithakm@users.noreply.github.com>
2025-02-27 23:06:32 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvimo4vYClUM0PJEM5M4vrZq4jTw6jL_8",
-  authDomain: "neww-d3702.firebaseapp.com",
-  projectId: "neww-d3702",
-  storageBucket: "neww-d3702.firebasestorage.app",
-  messagingSenderId: "491563870492",
-  appId: "1:491563870492:web:2b37bbaf9729aae17eb2d9",
-  measurementId: "G-BD10NHQMH4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
